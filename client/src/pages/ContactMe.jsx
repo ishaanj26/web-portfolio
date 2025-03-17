653a270588bf7bc00408893882c2fc71b997ccd2
Ishaan Jain <ishaanj2612@gmail.com>
2025-03-18 05:09:45 +0530

    updates

--- client/src/pages/ContactMe.jsx
+++ client/src/pages/ContactMe.jsx
@@ -23,7 +23,7 @@ function ContactMe() {
         setIsSubmitting(true)
 
         try {
-            const response = await fetch("http://localhost:5000/submit", {
+            const response = await fetch("https://ishaan-jain-web-portfolio.onrender.com/submit", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formState),
