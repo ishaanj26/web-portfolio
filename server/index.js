52886e7c87d32e8800aa3bd7ee394d9d86530547
Ishaan Jain <ishaanj2612@gmail.com>
2025-03-18 05:58:39 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['https://ishaan-jain-web-portfolio.onrender.com', 'http://localhost:5173'],
+    origin: ['https://ishaan-jain.vercel.app', 'http://localhost:5173'],
     credentials: true
 }));
 app.use(express.json());
